4dde378569adbac6723e70abcd1d0fc5df11e5a6
eviekr <evelyn_krall@brown.edu>
2023-03-20 10:12:50 -0400

    fixed "I'm"

--- gtky.m
+++ gtky.m
@@ -22,6 +22,7 @@ french_ui = "ui";
 french_j = "j'";
 french_m = "m'";
 french_c = "c'";
+english_Imm = "i'm";
 %Spanish_letter_accents= [eaigu, aaigu, uaigu, oaigu, ntilde, accented_i];
 %English_indications = [english_the,english_I_space,english_I_ns];
 %French_indicators = [egrave, ecircumflex, agrave, acircumflex, ugrave, ucircumflex, cedilla, ocircumflex, icircumflex, french_ui, french_m, french_j];
@@ -82,7 +83,7 @@ prompt = ' ';
 
     spanish_phrases= [ntilde, "yo ", "llamo", "soy ", "estoy", "favorito","favorita", "mi ", " es ", "favorito", "favorita","gusta", "dia ", "festivo", "superpoder", "comida","libros", "películas", "más", "prefiero", "voy ", "genero","música", "sí", "té"];
     french_phrases = [egrave, ecircumflex, agrave, acircumflex, ugrave, ucircumflex, cedilla, ocircumflex, icircumflex, french_ui, french_m, french_j, french_c, "préférée", "préféré", "les ", "mon ", "suis", "je", "favori ", "serait", "une ", "le ", "est ", "ces ", "ce "];
-    english_phrases_all = [english_the,english_I_space,english_I_ns, "My ","favorite", "name"," want ", " is ", "from"," it " "superpower", "holiday", "food","prefer","books","movie","like","coffee","music ", "love", "I'"];
+    english_phrases_all = [english_Imm, english_the,english_I_space,english_I_ns, "my ","favorite", "name"," want ", " is ", "from"," it " "superpower", "holiday", "food","prefer","books","movie","like","coffee","music ", "love", "i'm "];
          total_count_Spanish=0;
          total_count_English= 0; 
          total_count_French=0; 
